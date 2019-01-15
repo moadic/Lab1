84b1e556a1fc2c36761bad466cc776375400f305
moadic <46724649+moadic@users.noreply.github.com>
2019-01-15 14:02:09 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,24 @@
+/***********************************************************************
+* Java Program Name: Hello Printer
+************************************************************************
+* Project/Program Description: Prints Heading and the String Hello
+* _____________________________________________________________________
+* Muhammad Abdullah
+* 1/15/19
+* CMSC 255 Section 002
+***********************************************************************/
+
+public class HelloPrinter {
+	public static void printHeading() {
+		System.out.println("Muhammad Abdullah");
+		System.out.println("Java Project Name: Hello Printer");
+		System.out.println("Project Description: Prints Heading and the String Hello");
+		System.out.println("Version Date: 1/15/19");
+		System.out.println("Course Number and Section: CMSC 255 Section 002");
+		System.out.println();
+	}
+public static void main(String[] args){
+	printHeading();
+	System.out.println("Hello");
+}
+}
